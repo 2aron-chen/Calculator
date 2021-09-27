64a82b6af9715bac825a3543aa4c61f01e2bb7c6
Aaron <kchen339@wisc.edu>
2021-09-26 23:07:25 -0500

    Add some onclick function.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,9 +2,58 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickPlus(View view){
+        EditText firstTextField = (EditText) findViewById(R.id.editTextFirstNumber);
+        EditText secondTextField = (EditText) findViewById(R.id.editTextSecondNumber);
+        String str1 = firstTextField.getText().toString();
+        int iOne=Integer.parseInt(str1);
+        String str2 = secondTextField.getText().toString();
+        int iTwo=Integer.parseInt(str2);
+        goToActivity2(iOne + iTwo);
+    }
+
+    public void clickMinus(View view){
+        EditText firstTextField = (EditText) findViewById(R.id.editTextFirstNumber);
+        EditText secondTextField = (EditText) findViewById(R.id.editTextSecondNumber);
+        String str1 = firstTextField.getText().toString();
+        int iOne=Integer.parseInt(str1);
+        String str2 = secondTextField.getText().toString();
+        int iTwo=Integer.parseInt(str2);
+        goToActivity2(iOne - iTwo);
+    }
+
+    public void clickTime(View view){
+        EditText firstTextField = (EditText) findViewById(R.id.editTextFirstNumber);
+        EditText secondTextField = (EditText) findViewById(R.id.editTextSecondNumber);
+        String str1 = firstTextField.getText().toString();
+        double dOne=Double.parseDouble(str1);
+        String str2 = secondTextField.getText().toString();
+        double dTwo=Double.parseDouble(str2);
+        goToActivity2(dOne * dTwo);
+    }
+
+    public void clickDivide(View view){
+        EditText firstTextField = (EditText) findViewById(R.id.editTextFirstNumber);
+        EditText secondTextField = (EditText) findViewById(R.id.editTextSecondNumber);
+        String str1 = firstTextField.getText().toString();
+        double dOne=Double.parseDouble(str1);
+        String str2 = secondTextField.getText().toString();
+        double dTwo=Double.parseDouble(str2);
+        goToActivity2(dOne / dTwo);
+    }
+
+    public void goToActivity2(double d){
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("next", d);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/calculator/MainActivity2.java
+++ app/src/main/java/com/example/calculator/MainActivity2.java
@@ -2,13 +2,22 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textView2 = (TextView) findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        int i = intent.getIntExtra("next");
+        textView2.setText(i);
     }
 }
